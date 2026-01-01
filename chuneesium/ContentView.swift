c89ee529e4656bba0197b2894640b0e21d557401
Akasaka Ryuunosuke <local@local.local>
2026-01-01 21:02:59 +0900

    Initial Commit

---
+++ chuneesium/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  chuneesium
+//
+//  Created by DJ AKASAKA on 2026/01/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ chuneesium/chuneesiumApp.swift
@@ -0,0 +1,17 @@
+//
+//  chuneesiumApp.swift
+//  chuneesium
+//
+//  Created by DJ AKASAKA on 2026/01/01.
+//
+
+import SwiftUI
+
+@main
+struct chuneesiumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
